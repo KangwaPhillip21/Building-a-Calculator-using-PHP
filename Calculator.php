3638074bf03b9a8d0e81bfb6a116ce43201813af
KangwaPhillip21 <109252619+KangwaPhillip21@users.noreply.github.com>
2026-01-23 16:50:20 +0200

    Add basic calculator form in Calculator.php

---
+++ Calculator.php
@@ -0,0 +1,12 @@
+<HTML/>
+<form method ="post"action="">
+<input type = "text"name = "num1" placeholder = "first Number" required>
+<select name = "operation">
+<option value = "add">+</option>
+<option value = "Subtract"></option>
+<option value = "Multiply"></option>
+<option value = "divide"></option>
+</select>
+<input type = "text" name = "num2" placeholder = "Second Number" Required>
+<input type = "Submit" value= "Calculate">
+</form>
